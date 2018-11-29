1b7de3aaca8ad04028a1127bb24a29322af17630
Dusan Todorovic <dusantodorovic1989@gmail.com>
2018-11-29 21:09:18 +0100

    zadatak 3. ceo

--- src/components/AddCars.vue
+++ src/components/AddCars.vue
@@ -1,45 +1,59 @@
-<template >
-<div class="container">
-    <form submit="prevent">
-    <div class="form-group">
-    <input type="text" class="form-control" placeholder="Brand" v-model = "newCar.brand">
-    </div>
-
-    <div class="form-group">
-    <input type="text" class="form-control" placeholder="Model" v-model = "newCar.model">
-    </div>
-
-    <div class="form-group">
-    <input type="number" class="form-control" placeholder="Max Speed" v-model = "newCar.maxSpeed">
-    </div>
-
-    <div class="form-group">
-    <input type="number" class="form-control" placeholder="Number of doors" v-model = "newCar.numberOfDoors">
-    </div>
-
-     <div>
-    <select aria-placeholder="Select Section" v-model="newCar.year" :value="years">
-        
-        <option disabled value = ""> Select Year</option>
-        <option v-for="(year, index) in years" :key="index"> 
-            {{year}}
-        </option>
-    </select>
-    </div>
-
- <div class="custom-control">
-      <input type="checkbox" v-model="newCar.isAutomatic">
-      <label class="custom-control">Automatic</label>
-    </div>
-
-  <!-- <div>
- <button type="submit" class="btn btn-primary" @click="addCar">Submit</button>
- </div> -->
-    </form>
-</div>
+<template>
+   <div class="container">
+       <div class="container mt-3 mt-sm-5">
+           <div class="row justify-content-center">
+               <div class="col-md-6">
+                   <form @submit.prevent="addCars">
+                       <div class="form-group">
+                           <input type="text"  class="form-control" required minlength="2" placeholder="Brand" v-model="newCar.brand">
+                       </div>
+                       <div class="form-group">
+                           <input type="text"  class="form-control" placeholder="Model" required minlength="2" v-model="newCar.model">
+                       </div>
+                       <div class="form-group">
+                           <input type="number" class="form-control" placeholder="Max Speed" v-model="newCar.maxSpeed">
+                       </div>
+                       <div class="form-group">
+                           <input type="number"  class="form-control" placeholder="Number Of Doors" required v-model="newCar.numberOfDoors">
+                       </div>
+                       <div class="select">
+                           <select  aria-placeholder="Select Section" v-model="newCar.year" :value="years" required >
+                               <option disabled value="">Select year</option>
+                               <option v-for="(year, index) in years" :key="index">
+                                   {{ year }}
+                               </option>
+                           </select>
+                       </div>
+                       <div class="custom-control ">
+                           <input type="checkbox" v-model="newCar.isAutomatic" >
+                           <label class="custom-controll">Automatic</label>
+                       </div>
+                       <div class="validate-radio">
+                           <div class="engine-input">
+                               <label>Disel</label>
+                               <input type="radio" v-model="newCar.engine" value="Disel">
+                               <label>Petrol</label>
+                               <input type="radio" v-model="newCar.engine" value="Petrol">
+                               <label>Electric</label>
+                               <input type="radio" v-model="newCar.engine" value="Electric">
+                               <label>Hybrid</label>
+                               <input type="radio" v-model="newCar.engine" value="Hybrid">
+                           </div>
+                       </div>                        
+                       <button type="submit" class="btn btn-primary">Submit</button>
+                       <button  class="btn btn-info" @click.stop.prevent="previewCar">Preview</button>
+                       <button  class="btn btn-danger" @click.stop.prevent="resetForm">Reset</button>
+                   </form>
+               </div>
+           </div>
+       </div>
+   </div>
 </template>
 
+
 <script>
+import CarsService from "../services/CarsServices";
+
 export default {
     data (){
         return {
@@ -47,6 +61,18 @@ export default {
              newCar:{
                   isAutomatic: false
              }
+        }
+    },
+    methods:{
+        addCars(){
+            console.log('FGDFGDFG');
+            CarsService.add(this.newCar)
+                .then(response =>{
+                    this.newCar = {}
+                    this.$router.push({path: '/cars'})
+                }).catch(error =>{
+                    console.log(error.response)
+                })
         }
     }   
 }  
--- src/services/CarsServices.js
+++ src/services/CarsServices.js
@@ -8,6 +8,9 @@ class CarsServices {
        return axios.get('cars')
             
     }
+    add(newCar){
+        return axios.post('cars',newCar)
+    }
 }
 
 export const cars = new CarsServices();
